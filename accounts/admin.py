893432fcb2cecba7997781a6d0958942f4e31658
Enes <eneslucker@gmail.com>
2025-06-27 19:40:42 +0200

    reset pw function

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from authemail.admin import EmailUserAdmin
 
-# Register your models here.
+User = get_user_model()
+
+# Standard-Eintrag entfernen …
+admin.site.unregister(User)
+# … und mit Authemail-Admin wieder hinzufügen
+admin.site.register(User, EmailUserAdmin)
